c6b7703f564b04f916a82746e02cce3817020ae4
Tara <hymerstara@gmail.com>
2024-06-03 22:48:54 -0600

    fix: apply ai formatting for jsx

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,20 +4,20 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({ photo }) => {
   return (
     <div className="photo-list__item">
-      <img
-        src={photo.profile}
-        alt={`${photo.username}'s profile`}
-        className="photo-list__user-profile"
-      />
       <div className="photo-list__user-details">
-        <h4 className="photo-list__user-info">{photo.username}</h4>
         <img
-          src={photo.imageSource}
-          alt={`Photo ${photo.id}`}
-          className="photo-list__image"
+          src={photo.profile}
+          alt={`${photo.username}'s profile`}
+          className="photo-list__user-profile"
         />
-        <p className="photo-list__user-location">{`${photo.location.city}, ${photo.location.country}`}</p>
+        <h4 className="photo-list__user-info">{photo.username}</h4>
       </div>
+      <img
+        src={photo.imageSource}
+        alt={`Photo ${photo.id}`}
+        className="photo-list__image"
+      />
+      <p className="photo-list__user-location">{`${photo.location.city}, ${photo.location.country}`}</p>
     </div>
   );
 };
