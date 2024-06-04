dbe410461533562030149c4fdca6445d13424f89
Tara <hymerstara@gmail.com>
2024-06-04 00:14:28 -0600

    feat:add the favorite icon

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,23 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState } from "react";
 
-import FavIcon from './FavIcon';
-import '../styles/PhotoFavButton.scss';
+import FavIcon from "./FavIcon";
+import "../styles/PhotoFavButton.scss";
 
 function PhotoFavButton() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive((prevActive) => !prevActive);
+  };
+
   return (
-    <div className="photo-list__fav-icon">
+    <div
+      className={`photo-list__fav-icon ${isActive ? "active" : ""}`}
+      onClick={handleClick}
+    >
       <div className="photo-list__fav-icon-svg">
         {/* Insert React */}
+        <FavIcon />
       </div>
     </div>
   );
