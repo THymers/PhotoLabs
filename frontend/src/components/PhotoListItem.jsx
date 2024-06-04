d6c1ee8f8fe7d56d3aa05b924752a6cb5e1323d1
Tara <hymerstara@gmail.com>
2024-06-03 23:46:46 -0600

    style:align classes in CSS

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,20 +4,22 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({ photo }) => {
   return (
     <div className="photo-list__item">
-      <div className="photo-list__user-details">
-        <img
-          src={photo.profile}
-          alt={`${photo.username}'s profile`}
-          className="photo-list__user-profile"
-        />
-        <h4 className="photo-list__user-info">{photo.username}</h4>
-      </div>
       <img
         src={photo.imageSource}
         alt={`Photo ${photo.id}`}
         className="photo-list__image"
       />
-      <p className="photo-list__user-location">{`${photo.location.city}, ${photo.location.country}`}</p>
+      <div className="photo-list__user-details">
+        <div className="firstLine">
+          <img
+            src={photo.profile}
+            alt={`${photo.username}'s profile`}
+            className="photo-list__user-profile"
+          />
+          <h4 className="photo-list__user-info">{photo.username}</h4>
+        </div>
+        <p className="photo-list__user-location">{`${photo.location.city}, ${photo.location.country}`}</p>
+      </div>
     </div>
   );
 };
