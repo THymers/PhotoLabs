1fca6462f355e2c485674aeabfa5c1ce50b6b33f
Tara <hymerstara@gmail.com>
2024-06-04 23:13:01 -0600

    feat: refactor with mock data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,15 @@
 import React from "react";
 import HomeRoute from "./components/HomeRoute";
+import photos from "./mocks/photos"; // Import mock photos data
+import topics from "./mocks/topics";
 import "./App.scss";
-
-const sampleTopics = [
-  { id: "1", slug: "topic-1", title: "Nature" },
-  { id: "2", slug: "topic-2", title: "Travel" },
-  { id: "3", slug: "topic-3", title: "People" },
-];
 
 const App = () => {
   return (
     <div className="App">
-      <HomeRoute topics={sampleTopics} /> {/* Pass topics to HomeRoute */}
+      <HomeRoute topics={topics} photos={photos} />
     </div>
   );
 };
 
 export default App;
-
-// import React from "react";
-// import PhotoList from "./components/PhotoList";
-// import TopNavigationBar from "./components/TopNavigationBar";
-// import "./App.scss";
-
-// const App = () => {
-//   const isFavPhotoExist = true;
-//   return (
-//     <div className="App">
-//       <TopNavigationBar
-//         topics={sampleTopics}
-//         isFavPhotoExist={isFavPhotoExist}
-//       />
-//       <PhotoList />
-//     </div>
-//   );
-// };
-
-// export default App;
