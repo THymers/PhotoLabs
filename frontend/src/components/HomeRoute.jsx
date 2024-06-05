889584162c8ad27a37aea38f519b69354b39c418
Tara <hymerstara@gmail.com>
2024-06-05 00:26:50 -0600

    store favorite photos

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import TopNavigation from "./TopNavigationBar";
 import PhotoList from "./PhotoList";
 import "../styles/HomeRoute.scss";
 
 const HomeRoute = ({ topics, photos }) => {
+  const [favouritePhotos, setFavouritePhotos] = useState([]);
+  const toggleFavourite = (photoId) => {
+    if (favouritePhotos.includes(photoId)) {
+      setFavouritePhotos(favouritePhotos.filter((id) => id !== photoId));
+    } else {
+      setFavouritePhotos([...favouritePhotos, photoId]);
+    }
+  };
   return (
     <div className="home-route">
-      <TopNavigation topics={topics} />{" "}
-      {/* Passing topics to TopNavigationBar */}
-      <PhotoList photos={photos} /> {/* Passing photos to PhotoList */}
+      <TopNavigation topics={topics} />
+      <PhotoList photos={photos} />
     </div>
   );
 };
